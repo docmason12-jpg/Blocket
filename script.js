7e17c5d36902a35ee17fc1ec3a4f062a18d04613
docmason12-jpg <docmason12@gmail.com>
2026-08-21 21:04:33 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1413,20 +1413,22 @@ async function signup() {
 
     try {
 
-        const {
-            data,
-            error
-        } =
-            await supabaseClient.auth.signUp({
-    email: email,
-    password: password,
-    options: {
-        emailRedirectTo: "https://docmason12-jpg.github.io/Blocket/",
-        data: {
-            username: username
+const { data, error } =
+    await supabaseClient.auth.signUp({
+
+        email: email,
+
+        password: password,
+
+        options: {
+
+            data: {
+                username: username
+            }
+
         }
-    }
-});
+
+    });
 
 
         if (error) {
